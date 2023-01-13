6586aab33e5c2ed0f1c00ea325ec76e7a3462eb5
Lucas Talabera <talaberalu96@gmail.com>
2023-01-12 21:52:23 -0300

    segunda entrega de proyecto

---
+++ proyecto2.js
@@ -0,0 +1,12 @@
+var nombre = 'Lucas Talabera';
+var edad = '26';
+var desarrollador = true;
+var miCumpleaños = new Date("1996-09-05");
+const libroFavorito ={
+    nombre:'Padre rico Padre pobre',
+    autor: 'Robert Kiyosaki',
+    fecha: 1997,
+    url:'https://www.unpa.edu.mx/~blopez/algunosLibros/Padre-Rico-Padre-Pobre.pdf'
+
+}
+console.log(libroFavorito)
