d074baaf22e2ec676dbf0a972d555495fabbaa37
Lucas Talabera <talaberalu96@gmail.com>
2023-01-24 11:01:52 -0300

    entrega 4 actualizada (strings)

--- strings.js
+++ strings.js
@@ -2,6 +2,7 @@ let nombre = 'lucas';
 let apellido = 'talabera';
 let estudiante = `${nombre} ${apellido}`;
 let estudianteMayus = estudiante.toLocaleUpperCase();
+let estudianteMinus = estudiante.toLocaleLowerCase();
 let estudianteLetras = estudiante.length;
 let estudianteInicio = estudiante.charAt(0);
 let estudianteFinal = estudiante.slice(-1);
