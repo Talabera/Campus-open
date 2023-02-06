dfd6cb35404f687ffc94037228ee9c84ae34c77e
Lucas Talabera <talaberalu96@gmail.com>
2023-02-06 12:42:33 -0300

    entrega 5 numbres

---
+++ Numbers.js
@@ -0,0 +1,6 @@
+let altura = 183;
+let alturaMetros = 1.83 ;
+let kilogramos = 95.70;
+let alturaRedonda = alturaMetros.toPrecision(3);
+let kilogramosPreciso = kilogramos.toPrecision(2);
+let max_valor_JS= (2**1024);
